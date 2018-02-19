4fe5a2d4b0bdd57b9f9718cd274d045157892219
madhiarasi <31625386+madhiarasi@users.noreply.github.com>
2018-02-19 07:43:54 +0000

    Update and rename odev.java to oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class oddeven
+{
+public static void main(String args[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+if(a%2==0)
+System.out.println("Even");
+else 
+System.out.println("Odd");
+}
+}
