280c8f36ba218215e54675351937bf3591ad36af
madhiarasi <31625386+madhiarasi@users.noreply.github.com>
2018-02-19 07:38:04 +0000

    Create posti.java

---
+++ posti.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class posti
+{
+public static void main(String args[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+if(a>0)
+System.out.println("Positive");
+else if(a<0)
+System.out.println("Negative");
+else
+System.out.println("zero");
+}
+}
