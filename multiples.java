372f00dcdc3199ffa8f867d7037a5d9529786217
madhiarasi <31625386+madhiarasi@users.noreply.github.com>
2018-02-19 17:52:38 +0530

    Create multiples.java

---
+++ multiples.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class multiples
+{ 
+public static void main(String args[])
+{
+int f, i;
+Scanner sc=new Scanner(System.in);
+f=sc.nextInt();
+for(i=1;i<=f;i++)	
+{
+System.out.print(f*i+" ");
+}
+}
+}
