a56b7d797953af48775393db4948e3335ac92bca
madhiarasi <31625386+madhiarasi@users.noreply.github.com>
2018-02-19 07:46:08 +0000

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class vowels
+{
+public static void main(String args[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+a=sc.next();
+if(a=='a'||'A'||a=='e'||'E'||a=='I'||'I'||a=='o'||'O'||a=='u'||'U'))
+System.out.println("Vowel");
+else if((a>='a'&&a<='z')||(a>='A'&&a<='Z')
+System.out.println("Consonant");
+}
+}
